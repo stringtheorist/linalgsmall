27864b54ed9e124f9ff48f5b86f16cb539b1af07
Aftab Patel <aftab26989@gmail.com>
2011-09-03 14:06:58 +0530

    Matrix multiplication draft completed.

--- arithmetic.c
+++ arithmetic.c
@@ -48,6 +48,7 @@ matrix *multiply_matrices(matrix *mat1, matrix *mat2)
  	matrix *mat3;
 	int dim_x1, dim_y1, dim_x2, dim_y2;
 	int i, j, k;
+	double dot_i_j;
 	
 	dim_x1 = mat1->dimension_x;
 	dim_y1 = mat1->dimension_y;
@@ -64,5 +65,17 @@ matrix *multiply_matrices(matrix *mat1, matrix *mat2)
 
 	mat3->M = (double *)malloc(dim_x1*dim_y2*sizeof(double));
 	
+	
+	for(j = 0; j<dim_y2; j++) {
+		for(i = 0; i<dim_x1; i++) {
+			dot_i_j = 0;
+			for(k = 0; k<dim_x2; k++) {
+				dot_i_j += ((mat1->M)[i+(dim_y1*k)])*((mat2->M)[k+(dim_y2*j)]);
+			}
+			(mat3->M)[i+(dim_y2*j)] = dot_i_j;
+		}
+	}
 
 
+	return mat3;
+}
