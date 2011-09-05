90ac408ab838f9c047e415d135b2c6b243e4785e
Aftab Patel <aftab26989@gmail.com>
2011-09-05 23:55:37 +0530

    Minor corrections made to row_test.c

--- row_test.c
+++ row_test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "matrix.h"
 #include "arithmetic.h"
-
+#include <time.h>
 
 int main() {
 	
@@ -13,6 +13,8 @@ int main() {
 	dim_y = 5;
 
 
+
+	srand(time(NULL));
 	mat = gen_rand_matrix(10,5);
 	row = extract_row(mat, 5); 
 
@@ -22,6 +24,8 @@ int main() {
 	printf("row 5\n");
 	display_matrix(row);
 
+	destroy_matrix(row);
+	destroy_matrix(mat);
 	return 0;
 }
 
